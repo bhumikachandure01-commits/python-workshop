ab7e33b313e25368852038418465cd1a096c636c
bhumikachandure01-commits <bhumikachandure01@gmail.com>
2026-09-25 07:52:42 +0000

    sum

--- student/day1/functions/student_functions.py
+++ student/day1/functions/student_functions.py
@@ -1,15 +1,32 @@
-# Day 1 - Python Fundamentals
 
-student_name = input("Enter student name:")
-marks_python = float(input("Enter marks for Python:"))
-marks_math = float(input("Enter marks for Mathematics:"))
-marks_comm = float(input("Enter marks for Communication:"))
+def input_student():
+    student_name=input("enter student name:")
+    marks_python=float(input("enter marks of python"))
+    marks_math=float(input("enter maks of math")) 
+    marks_comm=float(input("marks of communication"))
+    dict_student_info ={
+        "name":student_name,
+        "python_marks":marks_python,
+        "math_marks":marks_math,
+        "comm_marks":marks_comm,
+    }
+    #return student_name,marks_python,marks_math,marks_comm return dict_student_info
+    return dict_student_info
 
-#TODO:
-#Create a function to calculate the percentage
+#TODO
+def calculate_percentage(marks_python,marks_math,marks_comm):
+        total= (marks_comm+marks_math+marks_python)
+        percentage = (total/300)*100
+        #percentage = 0 
 
-percentage = 0
+if __name__=="__main__":
+        print("\n-- Result--")
+        student_info = input_student()
+        #name,python_m,math_m,comm_m =input_student()
+        print(student_info)
+        print("student:", student_info["name"])
+        #print("percentage", calculate_percentage())                                  
+        student_info["python_marks"]
+        student_info["math_marks"]
+        student_info["comm_marks"]
 
-print("\n -- Result --")
-print("Student:", student_name)
-print("Percentage",percentage)
