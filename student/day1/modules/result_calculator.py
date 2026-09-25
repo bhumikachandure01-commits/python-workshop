2a57a090fdf126327104b95222f4221a07622577
bhumikachandure01-commits <bhumikachandure01@gmail.com>
2026-09-25 10:42:03 +0000

    end of day 1

--- student/day1/modules/result_calculator.py
+++ student/day1/modules/result_calculator.py
@@ -1,9 +1,18 @@
 # TODO
-# Create a function called calculate_percentage()
+def calculate_percentage(marks_python,marks_math,marks_comm):
+        total= (marks_comm+marks_math+marks_python)
+        percentage = (total/300)*100
+        return percentage
+      
 
-# It should :
-# 1. Accept three marks
-# 2. Calculate the total
-# 3. Calculate the percentage
-# 4. Return the percentage
-
+def calculate_grade(percentage):
+    grade=none
+    if percentage>=80:
+        grade="A"
+    if percentage>=60:
+        grade="B"
+    if percentage>=40:
+        grade="c"
+    else:
+        grade="d"
+    return grade    
--- student/day1/modules/student_modules.py
+++ student/day1/modules/student_modules.py
@@ -1,20 +1,39 @@
-from modules.result_calculator import calculate_percentage
+from result_calculator import calculate_percentage
+# Day 1 - Python Fundamentals
 
+student_name = input("Enter student name:")
+marks_python = float(input("Enter marks for Python:"))
+marks_math = float(input("Enter marks for Mathematics:"))
+marks_comm = float(input("Enter marks for Communication:"))
+def input_student():
+    student_name=input("enter student name:")
+    marks_python=float(input("enter marks of python"))
+    marks_math=float(input("enter maks of math")) 
+    marks_comm=float(input("marks of communication"))
+    dict_student_info ={
+        "name":student_name,
+        "python_marks":marks_python,
+        "math_marks":marks_math,
+        "comm_marks":marks_comm,
+    }
+    #return student_name,marks_python,marks_math,marks_comm return dict_student_info
+    return dict_student_info
 
-student_name = input("Enter student name: ")
+#TODO:
+#Create a function to calculate the percentage
+#TODO
 
-marks_python = float(input("Enter Python marks: "))
-marks_math = float(input("Enter Mathematics marks: "))
-marks_comm = float(input("Enter Communication marks: "))
-
-# TODO:
-# Import the calculate_percentage function
-# from your module
-
-# TODO:
-# Calculate the percentage using the imported function
 
 percentage = 0
-print("\n--- Result ---")
-print(f"Student: {student_name}")
-print(f"Percentage: {percentage}")
+if __name__=="__main__":
+        print("\n-- Result--")
+        student_info = input_student()
+        #name,python_m,math_m,comm_m =input_student()
+        print(student_info)
+        print("student:", student_info["name"])
+        print("percentage", calculate_percentage(                                  
+        student_info["python_marks"],
+        student_info["math_marks"],
+        student_info["comm_marks"])
+        )
+
